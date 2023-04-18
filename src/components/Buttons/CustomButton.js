9314d78e67e87f8ee6e9afebbaa9bcda937d795a
BichLoan <21521083@gm.uit.edu.vn>
2023-04-18 21:14:07 +0700

    a

--- src/components/Buttons/CustomButton.js
+++ src/components/Buttons/CustomButton.js
@@ -35,5 +35,42 @@ class CustomButton extends React.Component {
 export default CustomButton;
 
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+
+  button: {
+    width: scale(275),
+    height: scale(56, 'height'),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: scale(16),
+    alignSelf: 'center',
+  },
+
+  buttonPrimary: {
+    backgroundColor: CUSTOM_COLOR.FlushOrange,
+  },
+
+  buttonSecondary: {
+    backgroundColor: CUSTOM_COLOR.Carnation,
+  },
+
+  textButton: {
+    fontFamily: FONT_FAMILY.Semibold,
+    fontSize: scale(16),
+  },
+
+  textButtonPrimary: {
+    color: CUSTOM_COLOR.White,
+  },
+
+  textButtonSecondary: {
+    color: CUSTOM_COLOR.White,
+  },
 });
